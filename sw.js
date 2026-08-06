c67ae2187b4109a4ba145f279b577e69ba40a1e6
mickeybao1972 <13816620500@163.com>
2026-08-07 02:59:00 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for ËÄÕïÌåÖÊ¹ËÎÊ PWA
-const CACHE_NAME='sizheng-pwa-v39';
+// Service Worker for ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê¹ï¿½ï¿½ï¿½ PWA
+const CACHE_NAME='sizheng-pwa-v40';
 const ASSETS = [
   './',
   './index.html',
