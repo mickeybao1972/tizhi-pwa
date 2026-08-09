5f284d3ec73266096dfcbb7591e27319d3969a1f
mickeybao1972 <13816620500@163.com>
2026-08-09 17:16:37 +0800

    v63c: 面诊图简化为12经络虚线标注，删除8宫格与经络列表文字区块

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 四诊体质顾问 PWA
-const CACHE_NAME='sizheng-pwa-v63b-mianzhen-merged';
+const CACHE_NAME='sizheng-pwa-v63c-mianzhen-clean';
 const ASSETS = [
   './',
   './index.html',
