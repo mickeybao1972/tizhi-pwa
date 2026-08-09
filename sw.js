7230272470b4860bfc5fab507b4196ad521530c3
mickeybao1972 <13816620500@163.com>
2026-08-09 23:57:28 +0800

    v64 社会关系总览图(十神x社会身份 两层网络+对照表, 中心随日主)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 四诊体质顾问 PWA
-const CACHE_NAME='sizheng-pwa-v63m-zhuxing-mobilefix';
+const CACHE_NAME='sizheng-pwa-v64-shehui-guanxi';
 const ASSETS = [
   './',
   './index.html',
