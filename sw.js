b3bdb94e70930781ec195e4fd09cbc5db1e1f9ba
mickeybao1972 <13816620500@163.com>
2026-08-09 18:23:15 +0800

    v63h: 面诊图修复（鼻子居中对称/脏腑映射按灵枢五色校正/加回经络面部循行说明）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 四诊体质顾问 PWA
-const CACHE_NAME='sizheng-pwa-v63f-mianzhen-refined';
+const CACHE_NAME='sizheng-pwa-v63h-mianzhen-fixed';
 const ASSETS = [
   './',
   './index.html',
