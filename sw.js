18974d263650bf28135be40259ae8c8f561b58a4
mickeybao1972 <13816620500@163.com>
2026-08-09 14:30:52 +0800

    v61: 面部脏腑分区图重排坐标-额区居中/耳肾竖排圈内/眼鼻分离/人中引线/下颌入脸

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 四诊体质顾问 PWA
-const CACHE_NAME='sizheng-pwa-v60-mianzhen-gouji';
+const CACHE_NAME='sizheng-pwa-v61-mianzhen-facemap';
 const ASSETS = [
   './',
   './index.html',
