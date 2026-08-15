4e0612764bd2d8c54cfac9bcf46055aae6e4a65e
mickeybao1972 <13816620500@163.com>
2026-08-15 14:09:21 +0800

    fix: nuke all SW + caches on install (V3.4.7)

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
-// Disabled - no SW to prevent cache poisoning
-self.addEventListener('install', e => self.skipWaiting());
+// Aggressive cleanup
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  // Clear all caches
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+});
 self.addEventListener('activate', e => {
-  e.waitUntil(self.registration.unregister());
-  return self.clients.claim();
+  e.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      self.registration.unregister(),
+      caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    ])
+  );
 });
