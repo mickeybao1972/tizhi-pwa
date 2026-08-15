dcf525cd46f3cbab7be79fc1bd4bd3a8491c801c
mickeybao1972 <13816620500@163.com>
2026-08-15 13:56:51 +0800

    V3.4.5: Inject auto-cache-kill script (kills old poisoned SW/JS cache)

--- sw.js
+++ sw.js
@@ -1,29 +1,6 @@
-// Service Worker for 四诊体质顾问 PWA
-const CACHE_NAME='sizheng-pwa-v95';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg',
-  './favicon.ico',
-  './lunar.js'
-];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
+// Disabled - no SW to prevent cache poisoning
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  // network-first, fallback to cache
-  e.respondWith(
-    fetch(e.request).then(r => {
-      if (r.ok && e.request.method === 'GET') {
-        const clone = r.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone)).catch(() => {});
-      }
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
+  e.waitUntil(self.registration.unregister());
+  return self.clients.claim();
 });
